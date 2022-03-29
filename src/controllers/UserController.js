99e9447046477471b2794a75999f1fb54e904fb3
Diego Brito <1992diego1992@gmail.com>
2022-03-28 22:05:31 -0300

    Implementação do CRUD

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,83 @@
 import User from '../models/User';
 
 class UserController {
+  // CREATE
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // INDEX
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // SHOW
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      return res.json(user);
+    } catch (e) {
+      return res.json(null);
+    }
+  }
+
+  // UPDATE
+  async update(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não existe!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuario não existe!'],
+        });
+      }
+      const novoDados = await user.update(req.body);
+      return res.json(novoDados);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // DELETE
+  async delete(req, res) {
+    try {
+      if (!req.params.id) {
+        return res.status(400).json({
+          errors: ['ID não existe!'],
+        });
+      }
+
+      const user = await User.findByPk(req.params.id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuario não existe!'],
+        });
+      }
+      await user.destroy();
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({
         errors: e.errors.map((err) => err.message),
       });
     }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,5 +4,9 @@ import UserController from '../controllers/UserController';
 const routes = new Router();
 
 routes.post('/', UserController.store);
+routes.get('/', UserController.index);
+routes.get('/:id', UserController.show);
+routes.put('/:id', UserController.update);
+routes.delete('/:id', UserController.delete);
 
 export default routes;
