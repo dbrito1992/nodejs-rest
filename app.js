55073c96d4a9be0bcaa302595c2d74fcf61d26f9
Diego Brito <1992diego1992@gmail.com>
2022-03-28 21:17:43 -0300

    Criando Usuario

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ import './src/database';
 
 import express from 'express';
 import homeRoute from './src/routes/homeRoutes';
+import userRoute from './src/routes/userRoutes';
 
 class App {
   constructor() {
@@ -21,6 +22,7 @@ class App {
 
   routes() {
     this.app.use('/', homeRoute);
+    this.app.use('/users/', userRoute);
   }
 }
 
---
+++ src/models/User.js
@@ -0,0 +1,53 @@
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        nome: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [3, 255],
+              msg: 'O nome tem que haver entre 5 e 255 caracteres!',
+            },
+          },
+        },
+        email: {
+          type: Sequelize.STRING,
+          defaultValue: '',
+          unique: {
+            msg: 'Error, e-mail já existe!',
+          },
+          validate: {
+            isEmail: {
+              msg: 'Email inválido!',
+            },
+          },
+        },
+        password_hash: {
+          type: Sequelize.STRING,
+        },
+        password: {
+          type: Sequelize.VIRTUAL,
+          defaultValue: '',
+          validate: {
+            len: {
+              args: [6, 50],
+              msg: 'A senha tem que haver entre 6 e 50 caracteres!',
+            },
+          },
+        },
+      },
+      {
+        sequelize,
+      },
+    );
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcrypt.hash(user.password, 8);
+    });
+    return this;
+  }
+}
