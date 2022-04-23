6842e3f4513869cf15244301b9e9a8bc42d05096
Diego Brito <1992diego1992@gmail.com>
2022-04-23 17:17:16 -0300

    Inserção do JWT

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const routes = new Router();
 
 routes.post('/', UserController.store);
-routes.get('/', UserController.index);
+routes.get('/', loginRequired, UserController.index);
 routes.get('/:id', UserController.show);
 routes.put('/:id', UserController.update);
 routes.delete('/:id', UserController.delete);
