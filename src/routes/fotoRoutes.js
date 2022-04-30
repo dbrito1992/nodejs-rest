56a7528a085714eda34ab9d36f99bd5dd97c7d47
Diego Brito <1992diego1992@gmail.com>
2022-04-29 23:25:05 -0300

    Tratando fotos

--- src/routes/fotoRoutes.js
+++ src/routes/fotoRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import fotoController from '../controllers/FotoController';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const routes = new Router();
 
-routes.post('/', upload.single('foto'), fotoController.store);
+routes.post('/', fotoController.store);
 
 export default routes;
